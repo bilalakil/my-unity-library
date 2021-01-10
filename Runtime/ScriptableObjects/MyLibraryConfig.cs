491dfab2efe5ea2370396518316887f8df52b5d9
Bilal Akil <mail@bilalakil.me>
2021-01-10 22:44:54 +1100

    rmv: Simple Relay moved to separate repo.
    Simple Relay client now at:
    https://github.com/bilalakil/simple-relay-unity

--- Runtime/ScriptableObjects/MyLibraryConfig.cs
+++ Runtime/ScriptableObjects/MyLibraryConfig.cs
@@ -11,15 +11,13 @@ using UnityEngine.Audio;
  * All settings under `testConfig` are only relevant in-editor, for testing.
  */
 
-[CreateAssetMenu(menuName = "MyLibrary/MyLibraryConfig", fileName = "MyLibraryConfig")]
+[CreateAssetMenu(menuName = "Config/MyLibraryConfig", fileName = "MyLibraryConfig")]
 public class MyLibraryConfig : ScriptableObject
 {
     public AudioMixer musicMixer;
     public string musicMasterVolumeKey = "MasterVolume";
     public AudioMixer soundMixer;
     public string soundMasterVolumeKey = "MasterVolume";
-    public string simpleRelayHTTPSURL; // https://abc.def.ghi.com:1234/Prod"
-    public string simpleRelayWSSURL; // "wss://abc.def.ghi.com:1234/Prod"
 
     public TestConfig testConfig;
 
