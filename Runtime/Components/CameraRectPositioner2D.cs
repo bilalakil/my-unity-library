5ecdcae03f4a5a3671686ae8ab37a223e4f9ff85
Bilal Akil <mail@bilalakil.me>
2021-01-29 21:38:09 +1100

    chg: Make CameraRectPositioner2D params public for script modification.

--- Runtime/Components/CameraRectPositioner2D.cs
+++ Runtime/Components/CameraRectPositioner2D.cs
@@ -1,58 +1,67 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
-public class CameraRectPositioner2D : MonoBehaviour {
-#pragma warning disable CS0649
-    [SerializeField] Rect _worldRect;
-    [SerializeField] RectTransform _uiRect;
-#pragma warning restore CS0649
+public class CameraRectPositioner2D : MonoBehaviour
+{
+    public Rect worldRect;
+    public RectTransform uiRect;
 
     Camera _cam;
     float _initialZ;
     float _initialSize;
 
-    Vector3[] _uiRectCorners = new Vector3[4];
+    Vector3[] uiRectCorners = new Vector3[4];
 
-    void Awake() {
+    void Awake()
+    {
         _cam = GetComponent<Camera>();
         _initialZ = transform.position.z;
         _initialSize = _cam.orthographicSize;
     }
 
-    void Update() {
+    void Update()
+    {
         Vector2 containerCentre;
-        if (_uiRect != null) {
+
+        if (uiRect != null)
+        {
             // Camera size/position influences screen -> world conversions,
             // so we need to keep them static for calculations.
             transform.position = new Vector3(0f, 0f, _initialZ);
             _cam.orthographicSize = _initialSize;
 
-            _uiRect.GetWorldCorners(_uiRectCorners);
-            var br = _uiRectCorners[0];
-            var tl = _uiRectCorners[2];
+            uiRect.GetWorldCorners(uiRectCorners);
+            var br = uiRectCorners[0];
+            var tl = uiRectCorners[2];
             var worldBR = _cam.ScreenToWorldPoint(br);
             var worldTL = _cam.ScreenToWorldPoint(tl);
 
-            var container = new Rect(worldBR.x, worldBR.y,
-                worldTL.x - worldBR.x, worldTL.y - worldBR.y);
+            var container = new Rect(
+                worldBR.x, worldBR.y,
+                worldTL.x - worldBR.x, worldTL.y - worldBR.y
+            );
             containerCentre = container.center;
 
-            var xFactor = container.width / _worldRect.width;
-            var yFactor = container.height / _worldRect.height;
+            var xFactor = container.width / worldRect.width;
+            var yFactor = container.height / worldRect.height;
 
             _cam.orthographicSize = Mathf.Max(
                 _initialSize / xFactor,
-                _initialSize / yFactor);
-        } else {
+                _initialSize / yFactor
+            );
+        }
+        else
+        {
             var aspectRatio = (float)Screen.width / Screen.height;
             containerCentre = Vector2.zero;
 
             _cam.orthographicSize = Mathf.Max(
-                _worldRect.width / aspectRatio,
-                _worldRect.height) * 0.5f;
+                worldRect.width / aspectRatio,
+                worldRect.height
+            ) * 0.5f;
         }
 
-        var offset = _worldRect.center - containerCentre;
+        var offset = worldRect.center - containerCentre;
         transform.position = new Vector3(offset.x, offset.y, _initialZ);
     }
 }
