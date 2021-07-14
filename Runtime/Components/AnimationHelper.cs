ce708b4f277e499fa56b11d5e059fcc380c35396
Bilal Akil <mail@bilalakil.me>
2021-07-14 10:15:02 +1000

    ver: 0.7.7. new: AnimationHelper.DisableObject and SetAnimatorBool; sample scripts for KVSBridge and KVSCloudOnceSyncer.

--- Runtime/Components/AnimationHelper.cs
+++ Runtime/Components/AnimationHelper.cs
@@ -4,9 +4,29 @@ namespace MyLibrary
 {
     public class AnimationHelper : MonoBehaviour
     {
+        Animator _animator;
+
         public void PlaySound(string sound) => SoundController.Play(sound);
         public void PlaySoundAtThisLocation(string sound) =>
             SoundController.PlayAtLocation(sound, transform.position);
         public void SelfDestruct() => Destroy(gameObject);
+        public void DisableObject() => gameObject.SetActive(false);
+
+        public void SetAnimatorBoolTrue(string boolName)
+        {
+            PrepareAnimator();
+            _animator.SetBool(boolName, true);
+        }
+        public void SetAnimatorBoolFalse(string boolName)
+        {
+            PrepareAnimator();
+            _animator.SetBool(boolName, false);
+        }
+
+        void PrepareAnimator()
+        {
+            if (_animator == null)
+                _animator = GetComponent<Animator>();
+        }
     }
 }
